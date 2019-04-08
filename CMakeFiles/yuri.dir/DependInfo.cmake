c9c311dd6d9b615ee34819c653b5e3b5286f59fb
Guanqun Zhou <gzhou@rhel6-64k.slac.stanford.edu>
2019-04-08 14:16:44 -0700

    working for rhel6-64

---
+++ CMakeFiles/yuri.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/nfs/slac/g/beamphysics/gzhou/Yuri/Readin.cpp" "/nfs/slac/g/beamphysics/gzhou/Yuri/CMakeFiles/yuri.dir/Readin.cpp.o"
+  "/nfs/slac/g/beamphysics/gzhou/Yuri/Sdomain.cpp" "/nfs/slac/g/beamphysics/gzhou/Yuri/CMakeFiles/yuri.dir/Sdomain.cpp.o"
+  "/nfs/slac/g/beamphysics/gzhou/Yuri/Writeout.cpp" "/nfs/slac/g/beamphysics/gzhou/Yuri/CMakeFiles/yuri.dir/Writeout.cpp.o"
+  "/nfs/slac/g/beamphysics/gzhou/Yuri/main.cpp" "/nfs/slac/g/beamphysics/gzhou/Yuri/CMakeFiles/yuri.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/yuri.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yuri.dir/main.cpp.o"
+  "CMakeFiles/yuri.dir/Readin.cpp.o"
+  "CMakeFiles/yuri.dir/Sdomain.cpp.o"
+  "CMakeFiles/yuri.dir/Writeout.cpp.o"
+  "yuri.pdb"
+  "yuri"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yuri.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
